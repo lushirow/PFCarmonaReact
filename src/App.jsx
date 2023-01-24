fb4471acc6221b2176da7b4860b2ccc900cf8ffe
lushirow <lucianocarmona07@gmail.com>
2023-01-24 02:27:32 -0300

    prueba1

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
 import NavBar from './components/NavBar/NavBar'
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
 
 function App() {
-
   return (
     <NavBar/>
-    
   )
 }
 
